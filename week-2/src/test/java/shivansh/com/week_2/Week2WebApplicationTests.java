83a138f80599b7f11e73fc55cc95c1d700600209
SHIVANSHgupta1509 <shivanshgupta1509@gmail.com>
2024-11-21 00:54:47 +0530

    ini commit

---
+++ week-2/src/test/java/shivansh/com/week_2/Week2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package shivansh.com.week_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
